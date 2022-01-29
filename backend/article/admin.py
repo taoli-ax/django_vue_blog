e927a1e54b4268058fed9b91316a167321f0835e
Tyrion-from-kingsland <52994586+taoli-ax@users.noreply.github.com>
2022-01-29 14:36:41 +0800

    add-category

--- backend/article/admin.py
+++ backend/article/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
+from .models import Article,Category
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
--- backend/user_info/serializers.py
+++ backend/user_info/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserDescSerializer(serializers.ModelSerializer):
     """于文章列表中引用的嵌套序列化器"""
 
